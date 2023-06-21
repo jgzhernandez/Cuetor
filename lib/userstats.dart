045e791c064d14ab3b2d384a68d4e32776e2edf9
Jett Hernandez <jettlaagg@gmail.com>
2023-06-21 20:55:53 +0800

    Remove back button from appbars and named the exercise cards.

--- lib/userstats.dart
+++ lib/userstats.dart
@@ -1,14 +1,29 @@
 import 'package:flutter/material.dart';
 
-class StatisticsPage extends StatelessWidget {
+class StatisticsPage extends StatefulWidget {
   const StatisticsPage({super.key});
 
   @override
+  State<StatisticsPage> createState() => _StatisticsPageState();
+}
+
+class _StatisticsPageState extends State<StatisticsPage> {
+  @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-        'Player Statistics',
-        style: TextStyle(fontSize: 24),
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: const Text('Statistics'),
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.all(20),
+          child: const Column(
+            children: [
+              SizedBox(height: 10),
+            ],
+          ),
+        ),
       ),
     );
   }
