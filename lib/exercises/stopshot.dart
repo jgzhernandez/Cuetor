786f41ade9813ddc287ed1345f5a95d4dff4b991
Jett Hernandez <jettlaagg@gmail.com>
2023-06-27 01:42:22 +0800

    Add polygon drawer for exercise cameras

--- lib/exercises/stopshot.dart
+++ lib/exercises/stopshot.dart
@@ -14,11 +14,13 @@ class _StopShotState extends State<StopShot> {
   late CameraController _cameraController;
   bool _isLoading = true;
   bool _isRecording = false;
+  List<Offset> _polygonVertices = [];
+  bool _isDrawingMode = false;
 
   _initCamera() async {
     final cameras = await availableCameras();
-    final back = cameras.firstWhere((camera) =>
-    camera.lensDirection == CameraLensDirection.back);
+    final back = cameras.firstWhere(
+            (camera) => camera.lensDirection == CameraLensDirection.back);
     _cameraController = CameraController(back, ResolutionPreset.max);
     await _cameraController.initialize();
     await _cameraController.lockCaptureOrientation();
@@ -31,13 +33,76 @@ class _StopShotState extends State<StopShot> {
       setState(() => _isRecording = false);
       final route = MaterialPageRoute(
         fullscreenDialog: true,
-        builder: (_) => VideoPreview(filePath: file.path, folder: 'stop_shot',),
+        builder: (_) => VideoPreview(
+          filePath: file.path,
+          folder: 'stop_shot',
+        ),
       );
       Navigator.push(context, route);
     } else {
-      await _cameraController.prepareForVideoRecording();
-      await _cameraController.startVideoRecording();
-      setState(() => _isRecording = true);
+      if (_polygonVertices.length == 6) {
+        await _cameraController.prepareForVideoRecording();
+        await _cameraController.startVideoRecording();
+        setState(() => _isRecording = true);
+      } else {
+        showDialog(
+          context: context,
+          builder: (_) => AlertDialog(
+            title: const Text('Invalid Polygon'),
+            content: const Text(
+                'Please draw a polygon with exactly 6 vertices before recording.'),
+            actions: [
+              TextButton(
+                onPressed: () => Navigator.pop(context),
+                child: const Text('OK'),
+              ),
+            ],
+          ),
+        );
+      }
+    }
+  }
+
+  void _startDrawingMode() {
+    setState(() {
+      _isDrawingMode = true;
+      _polygonVertices = [];
+    });
+  }
+
+  void _stopDrawingMode() {
+    setState(() {
+      _isDrawingMode = false;
+    });
+    if (_polygonVertices.length == 6) {
+      // Send the coordinates to the Python code for further processing
+      // Replace the print statement with your code to send the coordinates
+      print(_polygonVertices);
+    } else {
+      // Handle the case where the user didn't draw exactly 6 vertices
+      showDialog(
+        context: context,
+        builder: (_) => AlertDialog(
+          title: const Text('Invalid Polygon'),
+          content: const Text('Please draw a polygon with exactly 6 vertices.'),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: const Text('OK'),
+            ),
+          ],
+        ),
+      );
+    }
+  }
+
+  void _handleTapDown(TapDownDetails details) {
+    if (_isDrawingMode) {
+      setState(() {
+        final RenderBox box = context.findRenderObject() as RenderBox;
+        final Offset localOffset = box.globalToLocal(details.globalPosition);
+        _polygonVertices.add(localOffset);
+      });
     }
   }
 
@@ -63,13 +128,88 @@ class _StopShotState extends State<StopShot> {
         ),
       );
     } else {
-      return Center(
+      return GestureDetector(
+        onTapDown: _handleTapDown,
         child: Stack(
-          alignment: Alignment.bottomCenter,
           children: [
             CameraPreview(_cameraController),
-            Padding(
-              padding: const EdgeInsets.all(25),
+            CustomPaint(
+              painter: PolygonPainter(vertices: _polygonVertices),
+              size: Size.infinite,
+            ),
+            if (_isDrawingMode)
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: Container(
+                  color: Colors.black54,
+                  padding: const EdgeInsets.symmetric(vertical: 25.0),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text(
+                        'Tap to add vertices (${_polygonVertices.length}/6)',
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18.0,
+                        ),
+                      ),
+                      const SizedBox(height: 8.0),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          FloatingActionButton(
+                            backgroundColor: Colors.red,
+                            onPressed: () {
+                              setState(() {
+                                _polygonVertices.removeLast();
+                              });
+                            },
+                            child: const Icon(
+                              Icons.delete,
+                              size: 32.0,
+                            ),
+                          ),
+                          const SizedBox(width: 16.0),
+                          FloatingActionButton(
+                            backgroundColor: Colors.blue,
+                            onPressed: () {
+                              setState(() {
+                                _polygonVertices.clear();
+                              });
+                            },
+                            child: const Icon(
+                              Icons.restart_alt,
+                              size: 32.0,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            Positioned(
+              bottom: 25,
+              left: 25,
+              child: FloatingActionButton(
+                backgroundColor: _isDrawingMode ? Colors.green : Colors.green,
+                onPressed: () {
+                  if (_isDrawingMode) {
+                    _stopDrawingMode();
+                  } else {
+                    _startDrawingMode();
+                  }
+                },
+                child: Icon(
+                  _isDrawingMode ? Icons.done : Icons.edit,
+                  size: 32.0,
+                ),
+              ),
+            ),
+            Positioned(
+              bottom: 25,
+              right: 25,
               child: FloatingActionButton(
                 backgroundColor: Colors.red,
                 child: Icon(_isRecording ? Icons.stop : Icons.circle),
@@ -80,5 +220,34 @@ class _StopShotState extends State<StopShot> {
         ),
       );
     }
+  }
+}
+
+class PolygonPainter extends CustomPainter {
+  final List<Offset> vertices;
+
+  PolygonPainter({required this.vertices});
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint()
+      ..color = Colors.red
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 2.0;
+
+    if (vertices.length > 0) {
+      final path = Path();
+      path.moveTo(vertices[0].dx, vertices[0].dy);
+      for (int i = 1; i < vertices.length; i++) {
+        path.lineTo(vertices[i].dx, vertices[i].dy);
+      }
+      path.close();
+      canvas.drawPath(path, paint);
+    }
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
   }
 }
