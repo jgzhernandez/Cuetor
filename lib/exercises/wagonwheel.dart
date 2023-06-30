24d12f24078d0efdaacd9da606bbef6d4f66bb41
Jett Hernandez <jettlaagg@gmail.com>
2023-06-30 22:24:03 +0800

    Tweaked cameras and videopreview.dart

--- lib/exercises/wagonwheel.dart
+++ lib/exercises/wagonwheel.dart
@@ -1,7 +1,5 @@
 import 'package:camera/camera.dart';
-import 'package:cuetor/exercises/polygonpainter.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import '../videopreview.dart';
 
@@ -17,13 +15,18 @@ class _WagonWheelState extends State<WagonWheel> {
   late CameraController _cameraController;
   bool _isLoading = true;
   bool _isRecording = false;
-  List<Offset> _polygonVertices = [];
-  bool _isDrawingMode = false;
+
+  void _setLandscapeOrientation() {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.landscapeLeft,
+      DeviceOrientation.landscapeRight,
+    ]);
+  }
 
   _initCamera() async {
     final cameras = await availableCameras();
     final back = cameras.firstWhere(
-        (camera) => camera.lensDirection == CameraLensDirection.back);
+            (camera) => camera.lensDirection == CameraLensDirection.back);
     _cameraController = CameraController(back, ResolutionPreset.max);
     await _cameraController.initialize();
     await _cameraController.lockCaptureOrientation();
@@ -39,81 +42,20 @@ class _WagonWheelState extends State<WagonWheel> {
         builder: (_) => VideoPreview(
           filePath: file.path,
           folder: 'wagon_wheel',
-          polygonVertices: _polygonVertices,
         ),
       );
       Navigator.push(context, route);
     } else {
-      if (_polygonVertices.length == 4) {
-        await _cameraController.prepareForVideoRecording();
-        await _cameraController.startVideoRecording();
-        setState(() => _isRecording = true);
-      } else {
-        showDialog(
-          context: context,
-          builder: (_) => AlertDialog(
-            title: const Text('Invalid Polygon'),
-            content: const Text(
-                'Please draw a polygon with exactly 4 vertices before recording.'),
-            actions: [
-              TextButton(
-                onPressed: () => Navigator.pop(context),
-                child: const Text('OK'),
-              ),
-            ],
-          ),
-        );
-      }
-    }
-  }
-
-  void _startDrawingMode() {
-    setState(() {
-      _isDrawingMode = true;
-      _polygonVertices = [];
-    });
-  }
-
-  void _stopDrawingMode() {
-    setState(() {
-      _isDrawingMode = false;
-    });
-    if (_polygonVertices.length == 4) {
-      // Send the coordinates to the Python code for further processing
-      // Replace the print statement with your code to send the coordinates
-      if (kDebugMode) {
-        print(_polygonVertices);
-      }
-    } else {
-      // Handle the case where the user didn't draw exactly 6 vertices
-      showDialog(
-        context: context,
-        builder: (_) => AlertDialog(
-          title: const Text('Invalid Polygon'),
-          content: const Text('Please draw a polygon with exactly 4 vertices.'),
-          actions: [
-            TextButton(
-              onPressed: () => Navigator.pop(context),
-              child: const Text('OK'),
-            ),
-          ],
-        ),
-      );
-    }
-  }
-
-  void _handleTapDown(TapDownDetails details) {
-    if (_isDrawingMode) {
-      setState(() {
-        final RenderBox box = context.findRenderObject() as RenderBox;
-        final Offset localOffset = box.globalToLocal(details.globalPosition);
-        _polygonVertices.add(localOffset);
-      });
+      await _cameraController.prepareForVideoRecording();
+      await _cameraController.startVideoRecording();
+      setState(() => _isRecording = true);
     }
   }
 
   @override
   void initState() {
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.leanBack);
+    _setLandscapeOrientation();
     super.initState();
     _initCamera();
   }
@@ -135,88 +77,13 @@ class _WagonWheelState extends State<WagonWheel> {
         ),
       );
     } else {
-      return GestureDetector(
-        onTapDown: _handleTapDown,
+      return Center(
         child: Stack(
+          alignment: Alignment.bottomCenter,
           children: [
             CameraPreview(_cameraController),
-            CustomPaint(
-              painter: PolygonPainter(vertices: _polygonVertices),
-              size: Size.infinite,
-            ),
-            if (_isDrawingMode)
-              Align(
-                alignment: Alignment.bottomCenter,
-                child: Container(
-                  color: Colors.black54,
-                  padding: const EdgeInsets.symmetric(vertical: 25.0),
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Text(
-                        'Tap to add vertices (${_polygonVertices.length}/4)',
-                        style: const TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 18.0,
-                        ),
-                      ),
-                      const SizedBox(height: 8.0),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          FloatingActionButton(
-                            backgroundColor: Colors.red,
-                            onPressed: () {
-                              setState(() {
-                                _polygonVertices.removeLast();
-                              });
-                            },
-                            child: const Icon(
-                              Icons.delete,
-                              size: 32.0,
-                            ),
-                          ),
-                          const SizedBox(width: 16.0),
-                          FloatingActionButton(
-                            backgroundColor: Colors.blue,
-                            onPressed: () {
-                              setState(() {
-                                _polygonVertices.clear();
-                              });
-                            },
-                            child: const Icon(
-                              Icons.restart_alt,
-                              size: 32.0,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            Positioned(
-              bottom: 25,
-              left: 25,
-              child: FloatingActionButton(
-                backgroundColor: _isDrawingMode ? Colors.green : Colors.green,
-                onPressed: () {
-                  if (_isDrawingMode) {
-                    _stopDrawingMode();
-                  } else {
-                    _startDrawingMode();
-                  }
-                },
-                child: Icon(
-                  _isDrawingMode ? Icons.done : Icons.edit,
-                  size: 32.0,
-                ),
-              ),
-            ),
-            Positioned(
-              bottom: 25,
-              right: 25,
+            Padding(
+              padding: const EdgeInsets.all(25),
               child: FloatingActionButton(
                 backgroundColor: Colors.red,
                 child: Icon(_isRecording ? Icons.stop : Icons.circle),
--- lib/videopreview.dart
+++ lib/videopreview.dart
@@ -1,22 +1,22 @@
 import 'dart:io';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cuetor/training.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:video_player/video_player.dart';
-import 'exercises/polygonpainter.dart';
 
 class VideoPreview extends StatefulWidget {
   const VideoPreview({
     Key? key,
     required this.filePath,
     required this.folder,
-    required this.polygonVertices,
   }) : super(key: key);
 
   final String filePath;
   final String folder;
-  final List<Offset> polygonVertices;
 
   @override
   State<VideoPreview> createState() => _VideoPreviewState();
@@ -25,26 +25,34 @@ class VideoPreview extends StatefulWidget {
 class _VideoPreviewState extends State<VideoPreview> {
   late VideoPlayerController _videoPlayerController;
 
-  @override
-  void dispose() {
-    _videoPlayerController.dispose();
-    super.dispose();
+  void _setLandscapeOrientation() {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.landscapeLeft,
+      DeviceOrientation.landscapeRight,
+    ]);
   }
 
-  Future _initVideoPlayer() async {
-    final videoFile = File(widget.filePath);
-    _videoPlayerController = VideoPlayerController.file(videoFile);
-    await _videoPlayerController.initialize();
-    await _videoPlayerController.setLooping(true);
-    await _videoPlayerController.play();
+  Widget _buildVideoPlayer() {
+    if (_videoPlayerController != null && _videoPlayerController.value.isInitialized) {
+      return Center(child: AspectRatio(
+        aspectRatio: _videoPlayerController.value.aspectRatio,
+        child: VideoPlayer(_videoPlayerController),
+      ));
+    } else {
+      return const Center(
+        child: CircularProgressIndicator(),
+      );
+    }
   }
 
   Future uploadFile() async {
     UploadTask? uploadTask;
 
+    _videoPlayerController = VideoPlayerController.file(File(widget.filePath));
+
     // Upload video to firebase
     final videoFile = File(widget.filePath);
-    final path = 'files/videos/${widget.folder}/${DateTime.now()}.mp4';
+    final path = 'user/${FirebaseAuth.instance.currentUser?.uid}/files/videos/${widget.folder}/${DateTime.now()}.mp4';
     final ref = FirebaseStorage.instance.ref().child(path);
     uploadTask = ref.putFile(videoFile);
     final snapshot = await uploadTask.whenComplete(() {});
@@ -59,54 +67,51 @@ class _VideoPreviewState extends State<VideoPreview> {
     });
   }
 
+  void _initVideoPlayer() {
+    _videoPlayerController = VideoPlayerController.file(File(widget.filePath))
+      ..initialize().then((_) {
+        setState(() {
+          // Once video is initialized, start playing it
+          _videoPlayerController.setLooping(true);
+          _videoPlayerController.play();
+        });
+      });
+  }
+
+  @override
+  void initState() {
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.leanBack);
+    super.initState();
+    _setLandscapeOrientation();
+    _initVideoPlayer();
+  }
+
+  @override
+  void dispose() {
+    _videoPlayerController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: BottomAppBar(
-          elevation: 0,
-          color: Colors.black26,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              IconButton(
-                icon: const Icon(Icons.delete),
-                onPressed: () {
-                  if (kDebugMode) {
-                    print('delete file');
-                  }
-                  Navigator.of(context).pop();
-                },
-              ),
-              IconButton(
-                icon: const Icon(Icons.check),
-                onPressed: () {
-                  if (kDebugMode) {
-                    print('do something with the file');
-                  }
-                  uploadFile();
-                  Navigator.of(context).pop();
-                },
-              )
-            ],
-          )),
-      extendBodyBehindAppBar: true,
-      body: FutureBuilder(
-        future: _initVideoPlayer(),
-        builder: (context, state) {
-          if (state.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
-          } else {
-            return Stack(
-              children: [
-                VideoPlayer(_videoPlayerController),
-                CustomPaint(
-                  painter: PolygonPainter(vertices: widget.polygonVertices),
-                  size: Size.infinite,
-                ),
-              ],
-            );
-          }
+      body: Stack(
+        children: [
+          // Video Player Widget
+          _buildVideoPlayer(),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Save the polygon coordinates and perform necessary actions
+          uploadFile();
+          SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => const TrainingPage()),
+          );
         },
+        child: const Icon(Icons.check),
       ),
     );
   }
